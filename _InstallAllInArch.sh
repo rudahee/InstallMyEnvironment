59d7ee5d55ca4e1a40b1bcc4bcd5983c6886a337
Ruben <rudahee@gmail.com>
2020-11-09 09:33:57 +0100

    First commit

---
+++ _InstallAllInArch.sh
@@ -0,0 +1,9 @@
+pamac build datagrip
+pamac build code
+pamac build postman-bin
+pamac build spring-tool-suite
+
+
+sudo pacman -Syu jdk-openjdk docker eclipse-ecj python git npm opera chromium firefox calibre filezilla steam wireshark
+
+sudo npm install -g typescript
