9eb6f6621a1cb7c742c2fc22204640a7f3119627
Rubén <35822076+rudahee@users.noreply.github.com>
2020-11-19 14:14:55 +0100

    Update _InstallAllInArch.sh

--- _InstallAllInArch.sh
+++ _InstallAllInArch.sh
@@ -1,10 +1,17 @@
-pamac build datagrip
-pamac build code
-pamac build postman-bin
-pamac build spring-tool-suite
+pamac install --no-confirm datagrip
+pamac install --no-confirm code
+pamac install --no-confirm postman-bin
+pamac install --no-confirm spring-tool-suite
 
 
-sudo pacman -Syu jdk-openjdk docker eclipse-ecj python git npm opera chromium firefox calibre filezilla steam wireshark
+sudo pacman --noconfirm -Syu jdk-openjdk docker eclipse-ecj python git npm opera chromium firefox calibre filezilla steam wireshark
 
 sudo npm install -g typescript
 sudo npm install -g @angular/cli
+
+for i in {1..10}
+do
+  echo ""
+done
+
+echo "Hemos terminao <3"
